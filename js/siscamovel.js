cd36c5f37db244ec820471cea1b0a189bef68ec4
Eduardo Santos <edsantos213@gmail.com>
2021-11-06 15:02:52 -0300

    (siscamovel) index: log in: Mostrar / ocultar senha

--- js/siscamovel.js
+++ js/siscamovel.js
@@ -7,3 +7,25 @@ function mockRecuperarConta() {
     MOCK_RECUPERAR_CONTA = document.getElementById("recuperarConta").value;
     document.getElementById("valorMockRecuperarConta").innerHTML = MOCK_RECUPERAR_CONTA;
 }
+
+
+//Mostra / oculta a senha digitada
+let tipo = document.getElementById('senha');
+
+document.getElementById('pass').addEventListener('click', () => {
+    if(tipo.value){
+        tipo.type == 'password' ? tipo.type = 'text' : tipo.type = 'password';
+        tipo.focus()
+        document.getElementById('pass').style.display = 'none';
+        document.getElementById('text').style.display = 'inline-block';
+    }
+})
+
+document.getElementById('text').addEventListener('click', () => {
+    if(tipo.value) {
+        tipo.type == 'text' ? tipo.type = 'password' : tipo.type = 'text';
+        tipo.focus()
+        document.getElementById('text').style.display = 'none';
+        document.getElementById('pass').style.display = 'inline-block';
+    }
+})
