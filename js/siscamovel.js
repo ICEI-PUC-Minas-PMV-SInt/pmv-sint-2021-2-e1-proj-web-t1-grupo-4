da0a383817d893cf2976db50559069fc403b9ac3
lssndrs <alessandra.silva.1368664@sga.pucminas.br>
2021-11-09 20:35:12 -0300

    (siscamovel) Index: Mock de recuperação de senha

--- js/siscamovel.js
+++ js/siscamovel.js
@@ -1,1 +1,9 @@
 // SISCAMOVEL.js
+// Mock: recuperar senha
+
+let MOCK_RECUPERAR_CONTA = "";
+
+function mockRecuperarConta() {
+    MOCK_RECUPERAR_CONTA = document.getElementById("recuperarConta").value;
+    document.getElementById("valorMockRecuperarConta").innerHTML = MOCK_RECUPERAR_CONTA;
+}
